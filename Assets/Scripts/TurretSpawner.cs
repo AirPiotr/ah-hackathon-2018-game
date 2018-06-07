ac8a812b6718309b5bfa8a562f3a95fb9237a645
Marcin Horoszko <marcin.horoszko@airhelp.com>
2018-06-07 15:39:51 +0200

    having fun with my friend unity

---
+++ Assets/Scripts/TurretSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TurretSpawner : MonoBehaviour {
+  public GameObject[] turretPrefabs;
+  protected List<TurretSocket> turretSockets;
+ 
+	void Awake() {
+    turretSockets = GameObject.FindObjectsOfType<TurretSocket>().ToList();
+	}
+	
+	public void Spawn () {
+		turretSockets.ForEach(socket => socket.SpawnTurret(turretPrefabs[0]));
+	}
+}
