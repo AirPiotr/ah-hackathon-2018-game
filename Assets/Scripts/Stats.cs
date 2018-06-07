5cd057dd2003dbbd2473eede5e4cd082d4010565
Piotr Fic <>
2018-06-07 15:15:21 +0200

    generic stats

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+    public int currentHealth;
+    public int maxHealth = 100;
+
+    void Awake() {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        if (currentHealth <= maxHealth) {
+            Die();
+        }
+    }
+
+    public void Die() {
+        // TODO: die somehow
+    }
+}
