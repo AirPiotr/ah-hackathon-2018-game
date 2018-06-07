90c495c4837cf71385b8868657d2e2129c48e151
Piotr Fic <piotr.fic@it-piotrfic.local>
2018-06-07 13:45:57 +0200

    jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Rigidbody rigidbody;
+    public float jumpForce = 5f;
+
 	
 	// Update is called once per frame
     void Update () {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Jump(); 
+        }
+    }
+
+    void Move() {
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * 3f;
 
-        Debug.Log(x);
-
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+    }
+
+    void Jump() {
+        rigidbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
     }
 }
