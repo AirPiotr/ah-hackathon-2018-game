3898da387124d9b15980ef4b4f7526b2b2984dfb
Piotr Fic <>
2018-06-07 15:48:01 +0200

    camera rotation

--- Assets/Scripts/player/PlayerController.cs
+++ Assets/Scripts/player/PlayerController.cs
@@ -3,34 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    
+    public float movementSpeed = 15f;
 
-    public Rigidbody rigidbody;
-    public float jumpForce = 5f;
-    public float movementSpeed = 150f;
-    public float rotationSpeed = 3f;
+    private Rigidbody rigidbody;
 
 	void Start() {
-        rigidbody = GetComponentInChildren<Rigidbody>();		
+        rigidbody = GetComponentInChildren<Rigidbody>();
 	}
 
 	// Update is called once per frame
-	void Update () {
+    void Update () {
         Move();
-
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Jump(); 
-        }
     }
 
     void Move() {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * rotationSpeed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
 
-        transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
-    }
-
-    void Jump() {
-        rigidbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
     }
 }
