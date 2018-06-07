530080f90f4cb1709450f483da9ae87d65dadce8
Marcin Horoszko <marcin.horoszko@airhelp.com>
2018-06-07 14:31:23 +0200

    turret wip

---
+++ Assets/TurrentController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrentController : MonoBehaviour {
+  public Transform headTransform;
+  public float visionRange = 10.0f;
+  
+  
+}
